e11fa48f191d632f47f5a82caa8c84bae9ccf0e5
amelangad <magdalena.slodzinska@gmail.com>
2023-04-10 17:56:17 +0200

    add photos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import '../styles/Projects.css'
+import bigbag from '../assets/bigbag.jpg'
+import kamar from '../assets/kamar.jpg'
+import movies from '../assets/movies.png'
 
 
 export default function Projects() {
@@ -7,14 +10,17 @@ export default function Projects() {
   const projects = [
     {id: 1,
     title: 'Website of recycling Company',
+    img: bigbag,
     desc: 'React + Tailwind + PHP form'
     },
     {id: 2,
       title: 'Website of cleaning Company',
+      img : kamar,
       desc: 'HTML + SCSS + JS'
       },
       {id: 3,
         title: 'Movies App',
+        img: movies,
         desc: "React + Node.js form"
         },
         {id: 4,
@@ -61,6 +67,7 @@ return (
     <div className ="project__wrapper">
     {projects.map(project => <div className ="project" key = {project.id}>
       <h1 className ="project__title">{project.title}</h1>
+      <div className ="project__image"><img  className ="project__img" src = {project.img}/></div>
       <p className ="project__text">{project.desc}</p></div>)}
     </div>
       </section>
